3969f90df2da5ecfc2d4ff5e11d931a439805be4
ishwariya <ishwariya.sukumar@numentica-ui.com>
2023-10-20 14:57:23 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+
+import "./App.css";
+import { AboutUs } from "./pages/about/AboutUs";
+import { Shop } from "./pages/shop/Shop";
+import { Team } from "./pages/team/Team";
+import { ShopSingle } from "./pages/shopSingle/ShopSingle";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/shopsingle" element={<ShopSingle />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/team" element={<Team />} />
+      </Routes>
+    </Router>
   );
 }
 
